3eff7b30def53508ff64adf8535e88b81f074553
Sauce101 <michaelsaucedo443@gmail.com>
2021-06-26 17:58:08 -0700

    initial

---
+++ pages/about.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus,
+          sapiente, sunt magni expedita, nihil vitae facilis fugiat assumenda
+          voluptatibus quas consequuntur voluptate hic quasi. Magnam atque dicta
+          voluptatibus distinctio consequatur?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus,
+          sapiente, sunt magni expedita, nihil vitae facilis fugiat assumenda
+          voluptatibus quas consequuntur voluptate hic quasi. Magnam atque dicta
+          voluptatibus distinctio consequatur?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus,
+          sapiente, sunt magni expedita, nihil vitae facilis fugiat assumenda
+          voluptatibus quas consequuntur voluptate hic quasi. Magnam atque dicta
+          voluptatibus distinctio consequatur?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
